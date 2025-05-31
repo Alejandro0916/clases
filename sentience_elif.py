df4c40dc720d0a5b945ee38e62415d2d5a292f37
Alejandro Rojas <luisrojasvanegas.0916@gmail.com>
2025-05-31 17:12:28 -0500

    sentience

---
+++ sentience_elif.py
@@ -0,0 +1,14 @@
+a = int(input("ingrese un numero: "))
+b = int(input("ingrese un numero: "))
+
+ope = a*b
+
+if ope > 1:
+    print("C es mayo a 1 pero menor que 100")
+elif ope > 100:
+    print("C es mayor a 100 pero menor que 200")
+elif ope > 200:
+    print("C es mayor que 200 pero menor que 300")
+else:
+    print("C no cumple con las condiciones")
+    
